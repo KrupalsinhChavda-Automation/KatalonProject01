1c043fa5fc8d661505b0afa82e128fce1be71265
KrupalsinhChavda-Automation <krupalchavda3499@gmail.com>
2024-08-12 00:09:22 +0530

    marged new changes

---
+++ Test Suites/testgitapp.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testgitapp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>91d09be2-c8bb-4c55-92bf-4c39bc289328</testSuiteGuid>
+   <testCaseLink>
+      <guid>7296dfe0-e496-41fd-9b73-813ea9873b51</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>079d7293-44e7-4730-bfd1-95a346eca963</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/L001_Register User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4bbbb67-7fbb-4e0c-9009-9bfe30c4bb0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/L002_Login User with correct email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6f157887-d225-47fd-907f-b1ca17d05876</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2cc46757-34e9-4fce-9544-3991cd38d689</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>475a87e5-9b14-4780-8435-04d18a95c212</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/L002_Login User with incorrect email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2b0e435c-848c-4e3c-aa47-677de2daf041</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3bccaadf-12c1-4b76-a881-d02756f451a1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
